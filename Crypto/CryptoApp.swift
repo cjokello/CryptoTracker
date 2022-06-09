41340910d3ef35d6b2d6320855810e523bf316e4
Okello Ayai <cjokello@gmail.com>
2022-06-09 16:15:21 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Admin on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
